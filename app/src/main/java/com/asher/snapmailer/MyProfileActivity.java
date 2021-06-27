824135236b34c947d35a4db784ad446898759f20
Sakshat682 <56864491+Sakshat682@users.noreply.github.com>
2021-06-27 12:04:25 +0530

    Add Settings Layout

--- app/src/main/java/com/asher/snapmailer/MyProfileActivity.java
+++ app/src/main/java/com/asher/snapmailer/MyProfileActivity.java
@@ -2,7 +2,10 @@ package com.asher.snapmailer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MyProfileActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class MyProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_profile);
+
+
+
+        Button segbtn= findViewById(R.id.settingsBtn);
+        segbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MyProfileActivity.this, SettingsActivity.class)
+                        .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+            }
+        });
     }
+
 }
