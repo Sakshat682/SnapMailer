13f77ed71aac03777bb001240f7529da5231ad0b
Sakshat682 <56864491+Sakshat682@users.noreply.github.com>
2021-07-06 15:42:19 +0530

    UI update

--- app/src/main/java/com/asher/snapmailer/MyProfileActivity.java
+++ app/src/main/java/com/asher/snapmailer/MyProfileActivity.java
@@ -1,8 +1,5 @@
 package com.asher.snapmailer;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -11,6 +8,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.tasks.OnCompleteListener;
